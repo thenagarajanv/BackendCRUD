ae0e6336a1878b53a6eea0d3bfa1d000fdd92ed9
thenagarajanv <thenagarajanv@gmail.com>
2025-05-06 16:49:50 +0530

    route structure

--- main.js
+++ main.js
@@ -3,13 +3,30 @@ import express from "express";
 // const express = require("express");
 
 const app = express();
+const port  = 4000;
 
-app.get("/", (req,res) => {
+// G -> For reading movies
+app.get("/movies", (req,res) => {
     res.json({msg: 'hello world!'});
 });
 
-const port  = 4000;
+// P -> For creating movies
+app.post("/movies", () => {
 
+})
+
+// U -> For updating movies
+app.put(`/movies/:id`,() => {
+
+})
+
+// For -> For delete movies
+app.delete("/movie/:id",()=>{
+
+})
+
+// For checking server is live
 app.listen(port, ()=>{
     console.log( `The server is running at http://localhost: ${port}`);
 })
+
